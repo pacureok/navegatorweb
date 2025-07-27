f4c15c8c1ea51a5e604c201081e934ffbbb4648e
pacureok <okkpacur@gmail.com>
2025-07-26 22:51:53 -0600

    Update RelayCommand.cs

--- NavegadorWeb/Classes/RelayCommand.cs
+++ NavegadorWeb/Classes/RelayCommand.cs
@@ -1,3 +1,6 @@
+// This file defines a generic RelayCommand class that implements ICommand.
+// It allows binding UI elements to methods in ViewModels without creating custom command classes for each action.
+
 using System;
 using System.Windows.Input;
 
@@ -5,26 +8,41 @@ namespace NavegadorWeb.Classes
 {
     public class RelayCommand : ICommand
     {
-        private readonly Action<object?> _execute;
-        private readonly Predicate<object?>? _canExecute;
+        private readonly Action<object?> _execute; // Action to execute when the command is invoked
+        private readonly Predicate<object?>? _canExecute; // Predicate to determine if the command can be executed
 
+        // Event that is raised when the CanExecute state changes
         public event EventHandler? CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="execute">The action to execute when the command is invoked.</param>
+        /// <param name="canExecute">The predicate to determine if the command can be executed. Can be null if always executable.</param>
         public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
         public bool CanExecute(object? parameter)
         {
             return _canExecute == null || _canExecute(parameter);
         }
 
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
         public void Execute(object? parameter)
         {
             _execute(parameter);
