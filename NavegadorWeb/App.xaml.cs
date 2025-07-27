c791135c2c9b1219c4cbcd67579e4d75af034b90
pacureok <okkpacur@gmail.com>
2025-07-27 00:10:15 -0600

    Update App.xaml.cs

--- NavegadorWeb/App.xaml.cs
+++ NavegadorWeb/App.xaml.cs
@@ -1,13 +1,6 @@
-using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace NavegadorWeb
+protected override void OnStartup(StartupEventArgs e)
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+    string selectedLanguage = ConfigurationManager.AppSettings["UserPreferredLanguage"] ?? "es";
+    LanguageService.ApplyCulture(selectedLanguage);
+    base.OnStartup(e);
 }
