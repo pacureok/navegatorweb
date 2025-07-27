b9900b9bdf3b6541248ea19872088d5a0aadb382
pacureok <okkpacur@gmail.com>
2025-07-26 22:53:27 -0600

    Update ToolbarPosition.cs

--- NavegadorWeb/Classes/ToolbarPosition.cs
+++ NavegadorWeb/Classes/ToolbarPosition.cs
@@ -1,10 +1,15 @@
+// This file defines an enumeration for possible toolbar positions in the browser.
+
 namespace NavegadorWeb.Classes
 {
+    /// <summary>
+    /// Represents the possible positions for the browser's toolbar.
+    /// </summary>
     public enum ToolbarPosition
     {
-        Top,
-        Bottom,
-        Left,
-        Right
+        Top,    // Toolbar is at the top of the window
+        Bottom, // Toolbar is at the bottom of the window
+        Left,   // Toolbar is on the left side of the window
+        Right   // Toolbar is on the right side of the window
     }
 }
