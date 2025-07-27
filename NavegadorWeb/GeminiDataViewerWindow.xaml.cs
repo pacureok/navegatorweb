67747f4a2c312bb3c6a62ef22f11e58b0e9bfc24
pacureok <okkpacur@gmail.com>
2025-07-26 20:33:58 -0600

    Update GeminiDataViewerWindow.xaml.cs

--- NavegadorWeb/GeminiDataViewerWindow.xaml.cs
+++ NavegadorWeb/GeminiDataViewerWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Media.Imaging; // Asegúrate de tener esta referencia
 
 namespace NavegadorWeb
 {
