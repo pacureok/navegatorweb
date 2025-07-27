c1bcfcba629f9726aad383bbbef283d11a0dc40e
pacureok <okkpacur@gmail.com>
2025-07-26 22:56:53 -0600

    Update TabGroupState.cs

--- NavegadorWeb/Classes/TabGroupState.cs
+++ NavegadorWeb/Classes/TabGroupState.cs
@@ -1,13 +1,19 @@
+// This file defines a class to represent the state of a tab group for session restoration.
+
 using System;
 using System.Collections.Generic;
 
 namespace NavegadorWeb.Classes
 {
+    /// <summary>
+    /// Represents the serializable state of a single tab group,
+    /// including its ID, name, and the URLs of its tabs.
+    /// </summary>
     public class TabGroupState
     {
-        public string GroupId { get; set; } = Guid.NewGuid().ToString();
-        public string GroupName { get; set; } = "Default Group";
-        public List<string?> TabUrls { get; set; } = new List<string?>();
-        public string? SelectedTabUrl { get; set; }
+        public string GroupId { get; set; } = Guid.NewGuid().ToString(); // Unique ID for the tab group
+        public string GroupName { get; set; } = "Default Group"; // Name of the tab group
+        public List<string?> TabUrls { get; set; } = new List<string?>(); // List of URLs for tabs in this group
+        public string? SelectedTabUrl { get; set; } // The URL of the currently selected tab within this group
     }
 }
