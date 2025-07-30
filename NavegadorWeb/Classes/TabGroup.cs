8e816281cf7cd5202ee7102a9d89ced7b0c9a632
pacureok <okkpacur@gmail.com>
2025-07-29 18:53:23 -0600

    Update TabGroup.cs

--- NavegadorWeb/Classes/TabGroup.cs
+++ NavegadorWeb/Classes/TabGroup.cs
@@ -1,24 +1,15 @@
-// This file defines the TabGroup class, which represents a collection of browser tabs.
-// It allows for organizing tabs into logical groups.
-
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using NavegadorWeb.Classes; // For TabItemData
 
 namespace NavegadorWeb.Classes
 {
-    /// <summary>
-    /// Represents a group of browser tabs.
-    /// Implements INotifyPropertyChanged to enable data binding in the UI.
-    /// </summary>
     public class TabGroup : INotifyPropertyChanged
     {
-        public string GroupId { get; set; } // <--- ¡CORREGIDO! Ahora es de lectura y escritura para la serialización
+        public string GroupId { get; set; } // ¡CORREGIDO! Ahora es de lectura y escritura
         private string _groupName;
 
-        // Public property for the group's name with change notification
         public string GroupName
         {
             get => _groupName;
@@ -32,12 +23,10 @@ namespace NavegadorWeb.Classes
             }
         }
 
-        // ObservableCollection to hold tabs within this group, for UI binding
         public ObservableCollection<TabItemData> TabsInGroup { get; } = new ObservableCollection<TabItemData>();
 
         private TabItemData? _selectedTabItem;
 
-        // Public property for the currently selected tab within this group
         public TabItemData? SelectedTabItem
         {
             get => _selectedTabItem;
@@ -51,51 +40,34 @@ namespace NavegadorWeb.Classes
             }
         }
 
-        /// <summary>
-        /// Initializes a new instance of the TabGroup class with a specified group name and a unique ID.
-        /// </summary>
-        /// <param name="groupName">The name of the tab group.</param>
         public TabGroup(string groupName)
         {
-            GroupId = Guid.NewGuid().ToString(); // Assign a unique ID when a new group is created
+            GroupId = Guid.NewGuid().ToString();
             _groupName = groupName;
         }
 
-        /// <summary>
-        /// Adds a tab to this group.
-        /// </summary>
-        /// <param name="tab">The TabItemData to add.</param>
         public void AddTab(TabItemData tab)
         {
             TabsInGroup.Add(tab);
-            if (SelectedTabItem == null) // Automatically select the first tab added
+            if (SelectedTabItem == null)
             {
                 SelectedTabItem = tab;
             }
-            OnPropertyChanged(nameof(TabsInGroup)); // Notify UI that the collection changed
+            OnPropertyChanged(nameof(TabsInGroup));
         }
 
-        /// <summary>
-        /// Removes a tab from this group.
-        /// </summary>
-        /// <param name="tab">The TabItemData to remove.</param>
         public void RemoveTab(TabItemData tab)
         {
             TabsInGroup.Remove(tab);
-            if (SelectedTabItem == tab) // If the removed tab was selected, select another one
+            if (SelectedTabItem == tab)
             {
                 SelectedTabItem = TabsInGroup.FirstOrDefault();
             }
-            OnPropertyChanged(nameof(TabsInGroup)); // Notify UI that the collection changed
+            OnPropertyChanged(nameof(TabsInGroup));
         }
 
-        // Event for property change notification
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        /// <summary>
-        /// Raises the PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The name of the property that changed.</param>
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
