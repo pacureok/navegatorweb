e29750c6cae178ed2ad33dda1ffe9e51bf70c907
pacureok <okkpacur@gmail.com>
2025-08-09 15:52:58 -0600

    Update PasswordEntry.cs

--- NavegadorWeb/PasswordEntry.cs
+++ NavegadorWeb/PasswordEntry.cs
@@ -1,15 +1,12 @@
 using System;
 
-namespace NavegadorWeb
+namespace NavegadorWeb.Classes
 {
-    /// <summary>
-    /// Clase para representar una entrada de contraseña guardada.
-    /// </summary>
     public class PasswordEntry
     {
-        public string Url { get; set; } // URL del sitio web
-        public string Username { get; set; } // Nombre de usuario
-        public string EncryptedPassword { get; set; } // Contraseña cifrada
-        public DateTime LastUsed { get; set; } // Fecha de último uso para ordenar
+        public string Url { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime LastUsed { get; set; } = DateTime.Now;
     }
 }
