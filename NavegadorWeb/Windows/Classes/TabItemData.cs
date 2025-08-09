a5441f580c61d266121ac51c312251927c430527
pacureok <okkpacur@gmail.com>
2025-08-09 15:30:15 -0600

    Update TabItemData.cs

--- NavegadorWeb/Windows/Classes/TabItemData.cs
+++ NavegadorWeb/Windows/Classes/TabItemData.cs
@@ -6,24 +6,41 @@ using System.Windows.Media.Imaging;
 
 namespace NavegadorWeb.Classes
 {
-    // La clase ahora implementa IDisposable
+    // Ahora hereda de ObservableObject e implementa IDisposable
     public partial class TabItemData : ObservableObject, IDisposable
     {
+        [ObservableProperty]
+        private string? _title = "Nueva Pestaña";
+
+        [ObservableProperty]
+        private string? _url = "about:blank";
+
+        [ObservableProperty]
+        private BitmapImage? _favicon;
+
+        [ObservableProperty]
+        private bool _isLoading;
+
+        [ObservableProperty]
+        private bool _isReaderMode;
+
+        [ObservableProperty]
+        private bool _isSuspended;
+
+        [ObservableProperty]
+        private DateTime _lastActivity = DateTime.Now;
+
+        public string? LastSuspendedUrl { get; set; }
+
         public WebView2 WebViewInstance { get; }
 
-        private string? _title;
-        public string? Title
-        {
-            get => _title;
-            set => SetProperty(ref _title, value);
-        }
-
+        // El constructor requiere una instancia de WebView2
         public TabItemData(WebView2 webView)
         {
             WebViewInstance = webView;
         }
 
-        // Implementación del método Dispose() para liberar los recursos del WebView
+        // Método para liberar los recursos del WebView
         public void Dispose()
         {
             WebViewInstance?.Dispose();
